0f4bf7476a80117fd288f06807afdce59108fa22
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-06-23 22:47:13 -0700

    make post request add new entrys into database

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
-// var express = require('express')
-//   , patials = require('express-partials')
-//   , app = express();
-
-
-// app.use(partials());
 
 var express = require('express');
 var partials = require('express-partials');
 const bodyParser = require('body-parser');
+const db = require('./database/db')
 
 const app = express();
 
@@ -23,8 +18,15 @@ app.get('/api/cows',(req, res) => {
 })
 
 app.post('/api/cows', (req, res) => {
-  console.log(req.body);
-  res.status(200).end();
+  var name = req.body.name || 'unknown cow';
+  var description = req.body.description || 'no descriptionj given';
+
+  db.queryAsync(`
+    INSERT INTO cows (name, description) values ('${name}', '${description}');
+  `);
+  //res.body = req.body;
+  console.log('inserted name: ' + req.body.name + ' description: ' + req.body.description);
+  res.status(200).send(req.body);
 })
 
 module.exports = app;
--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const Promise = require ('bluebird')
 
+
 const connection = mysql.createConnection({
   user: 'root',
   password: ''
