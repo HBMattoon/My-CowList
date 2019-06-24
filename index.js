47466cffead26944f40072e056299ab35a768f1a
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-06-23 19:07:36 -0700

    create super basic server that responds to get and post requests

---
+++ index.js
@@ -0,0 +1,3 @@
+require('./server/app').listen(8000, '127.0.0.1');
+const port = 8000;
+console.log('Listening on port 8000...');
---
+++ server/app.js
@@ -0,0 +1,30 @@
+// var express = require('express')
+//   , patials = require('express-partials')
+//   , app = express();
+
+
+// app.use(partials());
+
+var express = require('express');
+var partials = require('express-partials');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+console.log('this is the apps');
+
+app.get('/',(req, res) => {
+  console.log(req.body);
+  res.status(200).send('this it a test')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).end();
+})
+
+module.exports = app;
